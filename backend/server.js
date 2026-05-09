afdfede322d988f9eb7dbd365d0d9cce486ac98f
aryan-printf <aryan.ahir@egrambook.com>
2026-05-09 17:42:53 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://images-picker.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // MongoDB Connection
