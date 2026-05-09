e5a85e1defc9171961799d208e514873e51eb108
aryan-printf <aryan.ahir@egrambook.com>
2026-05-09 17:08:10 +0530

    add dotenv for environment variable management and update MongoDB connection to use MONGO_URI

--- backend/server.js
+++ backend/server.js
@@ -2,69 +2,63 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import User from "./models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/facelogin");
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo Error:", err));
 
 function euclideanDistance(a, b) {
- return Math.sqrt(
-   a.map((x, i) => Math.pow(x - b[i], 2)).reduce((sum, now) => sum + now)
- );
+  return Math.sqrt(
+    a.map((x, i) => Math.pow(x - b[i], 2)).reduce((sum, now) => sum + now),
+  );
 }
 
 app.post("/register", async (req, res) => {
- try {
+  try {
+    const user = new User({
+      descriptor: req.body.descriptor,
+    });
 
-  const user = new User({
-    descriptor: req.body.descriptor
-  });
+    await user.save();
 
-  await user.save();
-
-  res.json({ status: "saved" });
-
- } catch (err) {
-  res.status(500).json({ error: err.message });
- }
+    res.json({ status: "saved" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.post("/login", async (req, res) => {
+  const { descriptor } = req.body;
 
- const { descriptor } = req.body;
+  const users = await User.find();
 
- const users = await User.find();
+  let bestDistance = 1;
 
- let bestDistance = 1;
+  for (let user of users) {
+    const distance = euclideanDistance(descriptor, user.descriptor);
 
- for(let user of users){
-
-  const distance = euclideanDistance(
-   descriptor,
-   user.descriptor
-  );
-
-  if(distance < bestDistance){
-
-   bestDistance = distance;
-
+    if (distance < bestDistance) {
+      bestDistance = distance;
+    }
   }
 
- }
+  if (bestDistance < 0.5) {
+    return res.json({ match: true });
+  }
 
- if(bestDistance < 0.5){
-
-  return res.json({match:true});
-
- }
-
- res.json({match:false});
-
+  res.json({ match: false });
 });
 
 app.listen(5000, () => {
- console.log("Server running on port 5000");
+  console.log("Server running on port 5000");
 });
